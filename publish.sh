5daac3d5ff23087fdf7e243c74a1bf3fd7366f76
dskprt <59660013+dskprt@users.noreply.github.com>
2021-04-05 09:10:56 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,3 +4,9 @@ dotnet publish nylium.Launch -c Release -r linux-x64 -p:PublishSingleFile=true -
 dotnet publish nylium.Launch -c Release -r linux-musl-x64 -p:PublishSingleFile=true --self-contained false
 dotnet publish nylium.Launch -c Release -r linux-arm -p:PublishSingleFile=true --self-contained false
 dotnet publish nylium.Launch -c Release -r linux-arm64 -p:PublishSingleFile=true --self-contained false
+7z a nylium.Launch/bin/Release/net5.0/win-x64.zip ./nylium.Launch/bin/Release/net5.0/win-x64/publish/*
+7z a nylium.Launch/bin/Release/net5.0/osx-x64.zip ./nylium.Launch/bin/Release/net5.0/osx-x64/publish/*
+7z a nylium.Launch/bin/Release/net5.0/linux-x64.zip ./nylium.Launch/bin/Release/net5.0/linux-x64/publish/*
+7z a nylium.Launch/bin/Release/net5.0/linux-musl-x64.zip ./nylium.Launch/bin/Release/net5.0/linux-musl-x64/publish/*
+7z a nylium.Launch/bin/Release/net5.0/linux-arm.zip ./nylium.Launch/bin/Release/net5.0/linux-arm/publish/*
+7z a nylium.Launch/bin/Release/net5.0/linux-arm64.zip ./nylium.Launch/bin/Release/net5.0/linux-arm64/publish/*
